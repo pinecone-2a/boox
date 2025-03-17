326e26e0c3a488096a0e5f624cdbbb51221b2762
24LP8796 24LP8796 <amgaa0329@gmail.com>
2025-03-17 14:36:17 +0800

    3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { SignIn, UserButton, useUser } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 export default function Home() {
   return <div></div>;
 }
