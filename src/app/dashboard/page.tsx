0e62773496a6219e141f1b039e1cf17119ce73a1
Kawabatajt <amgaa0329@gmail.com>
2025-03-18 15:50:12 +0800

    3 dashboard (#8)
    * dashboard
    
    * dashboardsan
    
    * dashboardsan
    
    * dashboardsan
    
    ---------
    
    Co-authored-by: 24LP8466 24LP8466 <24LP8466@G302-CC0088.local>

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,72 @@
-export default function Dashboard() {
-  return <div></div>;
+import * as React from "react";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+
+interface Book {
+  image: string;
+}
+
+interface BookSectionProps {
+  sectionName: string;
+  bookList: Book[];
+}
+
+export default function BookLists() {
+  const wishlistBooks: Book[] = [
+    { image: "image 10.png" },
+    { image: "image 11.png" },
+    { image: "image 12.png" },
+    { image: "image 8.jpg" },
+    { image: "image 9.png" },
+    { image: "image 10.png" },
+    { image: "image 11.png" },
+    { image: "image 12.png" },
+    { image: "image 8.jpg" },
+    { image: "image 9.png" },
+  ];
+
+  const matchesBooks: Book[] = [
+    { image: "image 8.jpg" },
+    { image: "image 9.png" },
+    { image: "image 10.png" },
+    { image: "image 11.png" },
+    { image: "image 12.png" },
+    { image: "image 8.jpg" },
+    { image: "image 9.png" },
+    { image: "image 10.png" },
+    { image: "image 11.png" },
+    { image: "image 12.png" },
+  ];
+
+  return (
+    <div>
+      <BookSection sectionName="Wishlist" bookList={wishlistBooks} />
+      <BookSection sectionName="Matches" bookList={matchesBooks} />
+    </div>
+  );
+}
+
+function BookSection({ sectionName, bookList }: BookSectionProps) {
+  return (
+    <div className="mb-6">
+      <h2 className="text-2xl font-bold mb-2">{sectionName}</h2>
+      <Carousel className="w-full h-[112px] ">
+        <CarouselContent>
+          {bookList.map((book: Book, index: number) => (
+            <CarouselItem key={index} className="basis-1/5 bg-yellow-400 p-2 ">
+              <img
+                src={book.image}
+                alt={`${sectionName} book ${index + 1}`}
+                className="w-[64px] h-[99px] object-cover rounded-xl shadow-lg ml-2"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </div>
+  );
 }
