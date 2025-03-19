4fc023e559bc7320eb22aa351e75b100a204bf86
cjganaa <ganaaeenee02@gmail.com>
2025-03-19 15:31:15 +0800

    fix layout and page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-<<<<<<< HEAD
-=======
 import { SignedIn } from "@clerk/nextjs";
 import SignupHandler from "./_components/signup";
-
->>>>>>> main
 export default function Home() {
   return (
     <div className="bg-cyan-950 flex justify-center items-center">
