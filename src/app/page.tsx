d183aa8c292da4172de9e75bb6d6b4d394f0f180
24LP8796 24LP8796 <amgaa0329@gmail.com>
2025-03-19 15:22:00 +0800

    3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,5 +4,11 @@ import { SignedIn } from "@clerk/nextjs";
 import SignupHandler from "./_components/signup";
 
 export default function Home() {
-  return <div className="bg-cyan-950 flex justify-center items-center"></div>;
+  return (
+    <div className="bg-cyan-950 flex justify-center items-center">
+      <SignedIn>
+        <SignupHandler />
+      </SignedIn>
+    </div>
+  );
 }
