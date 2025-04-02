b727cc9d930f03c3bf4a35657acd4df362135d16
cjganaa <ganaaeenee02@gmail.com>
2025-04-02 17:00:12 +0800

    delete my book

--- src/app/api/books/singlebook/route.ts
+++ src/app/api/books/singlebook/route.ts
@@ -61,3 +61,40 @@ export async function DELETE(req: NextRequest) {
     );
   }
 }
+export async function PATCH(req: NextRequest) {
+  try {
+    const id = req.nextUrl.searchParams.get("id");
+    if (!id) {
+      return NextResponse.json({ message: "NO_ID_PROVIDED" }, { status: 400 });
+    }
+    await prisma.book.update({
+        where: {
+          id
+        },
+        data: { status: "PASSIVE" },
+    });
+    await prisma.swipe.updateMany({
+      where: {
+        bookId:id
+      },
+      data: { status: "PASSIVE" },
+    });
+    await prisma.match.updateMany({
+      where: {
+          status: "PENDING",
+          OR: [
+            { like1: { bookId: id } },
+            { like2: { bookId: id } }
+          ],
+      },
+      data: { status: "REJECTED" },
+    });
+    return NextResponse.json({ message: "book deleted successfully" });
+  } catch (e) {
+    console.error("Error fetching book:", e);
+    return NextResponse.json(
+      { message: "INTERNAL_SERVER_ERROR" },
+      { status: 500 }
+    );
+  }
+}
